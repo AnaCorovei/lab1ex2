80c731def7ad97c9e86f66d889f92b5f6e056a32
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 11:33:52 +0300

    correct variablename

--- lab1ex2/Program.cs
+++ lab1ex2/Program.cs
@@ -10,18 +10,18 @@ namespace lab1ex2
             /* Ex2
             Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura*/
 
-            int firstNumber, secondtNumber, thirdNumber;
+            int firstNumber, secondNumber, thirdNumber;
 
             Console.WriteLine("Enter firstNumber: ");
             firstNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter secondtNumber: ");
-            secondtNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter secondNumber: ");
+            secondNumber = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter thirdNumber: ");
             thirdNumber = int.Parse(Console.ReadLine());
 
-            int mediaAritmetica = (firstNumber + secondtNumber + thirdNumber)/3;
+            int mediaAritmetica = (firstNumber + secondNumber + thirdNumber)/3;
             Console.WriteLine("Media Aritmetica este: " + mediaAritmetica);
         }
     }
